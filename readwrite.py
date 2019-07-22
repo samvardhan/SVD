b33ed177c2be99c0cdbbf5ba4b860fc128cec33c
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-07-22 12:59:22 -0400

    Create readwrite.py

---
+++ readwrite.py
@@ -0,0 +1,30 @@
+def detTimestepAndConfigNum(filename) 
+ 
+ tlast= -1
+ timeStepNum = 0
+ timeStepNum_last = -1
+ configNum = 0
+ 
+ with open ( filename, "r") as file:
+    for line in file:
+		t = int(line.split() [0])
+		
+		if t== (tlast+1):
+		timeStepNum += 1
+		tlast = t
+		
+		elif t==0:
+		     if timeStepNum_last>=0
+				 assert timeStepNum = timeStepNum_last, \ "Err(detTimestepAndConfigNum): No. of timesteps not consistent across configurations"
+		timeStepNum_last = timeStepNum
+		timeStepNum = 1
+		configNum += 1
+		tlast = t
+		
+		else:
+		   print(:"Err(detTimestepAndConfigNum): timestep in 1st column does not behave as expected")
+			 return -1
+			 
+ assert timeStepNum = timeStepNum_last, \ "Err(detTimestepAndConfigNum): No. of timesteps not consistent across configurations"
+ return timeStepNum, configNum
+		
