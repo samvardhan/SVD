3e063392fe5700d147b16ff69ccccb451860a43e
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-07-23 14:06:49 -0400

    Update physQuants.py

--- physQuants.py
+++ physQuants.py
@@ -1,7 +1,8 @@
+def KK(mEff, Qsq, L):
+  return np.sqrt( 2* energy(mEff,Q,L) * (energy(mEff, Q,L)+ mEff))
+
 def energy(mEff, Q, L):
   return np.sqrt(mEff**2 + (2.0 *np.pi /L)**2 * Qsq)
-
-
 
 def kineFactor(ratio_err, mEff, Q, L):
 momNum = ratio_err.shape[0]
@@ -16,5 +17,18 @@ for b in range(binNum)
     for q in range(momNum)
         Qsq = np.dot(Q[q], Q[q])
         
-        kineFactor[b, q]= [[ energy(mEff[b] ,Qsq, L) +mEff[b] 
+        kineFactor[b, q]= [[ (energy(mEff[b] ,Qsq, L) +mEff[b]), 0], \
+                                   [ 2.0 * np.pi / L * Q[ q, 0 ], 0 ], \
+                                   [ 2.0 * np.pi / L * Q[ q, 1 ], 0 ], \
+                                   [ 2.0 * np.pi / L * Q[ q, 2 ], 0 ], \
+                                   [ 0, 2.0 * np.pi / L * Q[ q, 2 ] ], \
+                                   [ 0, -2.0 * np.pi / L * Q[ q, 1 ] ], \
+                                   [ 0, -2.0 * np.pi / L * Q[ q, 2 ] ], \
+                                   [ 0, 2.0 * np.pi / L * Q[ q, 0 ] ], \
+                                   [ 0, 2.0 * np.pi / L * Q[ q, 1 ] ], \
+                                   [ 0, -2.0 * np.pi / L * Q[ q, 0 ] ] ] \
+                    / np.repeat[ ratio_err[q], 2).reshape(10,2) \
+                    / KK(mEff[b], Qsq, L)
+                                
+  return kineFactor
 
