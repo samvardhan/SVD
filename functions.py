0859da705d6767e5c8cee17da77150966b5903f8
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-07-28 12:41:50 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,16 +1,5 @@
 def processMomList (momLists)
- if momLists.ndim>2:
-    if len(momLists)>1:
-      momList_0 = momLists[0].flat
-      for m1 in momLists[1:]:
-       for i in range (m1.size):
-          assert m1.flat[i] == momList_0[i], "Momenta list do not match"
-          
-    momList = momLists[0]
-    
-  else
-  momList = momLists
-    
+
 Qsq_s= []
 Qsq_e= []
 Qsq = np.round((np.apply_along_axis(np.linalg.norm, 1, np.array(momList)))**2)
