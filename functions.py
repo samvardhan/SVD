1a8dd7fdafd8829593adc9d2983c1039f8d3a2ce
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-08-06 18:53:30 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -8,13 +8,13 @@ q_last = Qsq[0]
 Qsq_s.append(0)
 Qsq_e.append(0)
 
-for q in Qsq[1:]
+for q in Qsq[1:]:
    if q!= q_last:
-   assert q > q_last, "Momentum list not in ascending order"
-   Qsq_s.append(np.where(Qsq == q)[0][0])
-   Qsq_e.append(np.where(Qsq == q)[0][-1])
-   q_last = q; 
+      assert q > q_last, "Momentum list not in ascending order"
+      Qsq_s.append(np.where(Qsq == q)[0][0])
+      Qsq_e.append(np.where(Qsq == q)[0][-1])
+      q_last = q; 
    
    Qsq = sorted(list(set(Qsq)))
    
- return np.array(Qsq, dtype= int), np.array(Qsq_s), np.array(Qsq_e)
+return np.array(Qsq, dtype= int), np.array(Qsq_s), np.array(Qsq_e)
