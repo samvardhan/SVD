1f00f606a7361918136d2b478530a98e3948155d
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-08-07 16:43:55 -0400

    Update GeGmSVD.py

--- GeGmSVD.py
+++ GeGmSVD.py
@@ -65,8 +65,13 @@ for qsq in range(QsqNum):
     uT = np.transpose(u,(0,2,1))
     v = np.transpose(vT,(0,2,1))
     
-    temp_s= np.zeros((u.shape[-1],vT.shape[-2]))
+    s_mat= np.zeros((u.shape[-1],vT.shape[-2]))
+    s_mat_inv= np.zeros((binNum,) +  np.transpose(s[b]).shape
     
-
-
-
+    for b in range binNum
+        s_mat[:min(u.shape[-1],vT.shape[-2]),:min(u.shape[-1],vT.shape[-2])] = np.diag(s[b])
+        s_mat_inv[b] = np.linalg.pinv[s_mat]
+         
+    inv_mat = v @ s_mat_inv @ uT
+     
+    inverse[qsq] = np.average(inv_mat, axis=0).T
