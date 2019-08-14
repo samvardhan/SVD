785d1e7220b0ae000df0977297dd556007951cc0
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-08-14 10:54:43 -0400

    Update physQuants.py

--- physQuants.py
+++ physQuants.py
@@ -5,15 +5,15 @@ def energy(mEff, Q, L):
   return np.sqrt(mEff**2 + (2.0 *np.pi /L)**2 * Qsq)
 
 def kineFactor(ratio_err, mEff, Q, L):
-momNum = ratio_err.shape[0]
-ratioNum = ratio_err.shape[-1] #multiple error ratios?#
-binNum = len(mEff)
+ momNum = ratio_err.shape[0]
+ ratioNum = ratio_err.shape[-1] #multiple error ratios?#
+ binNum = len(mEff)
 
-assert len(Q) == momNum, "Err(kineFactor): dimension of ratio errors and momentum transfers do not match"
+ assert len(Q) == momNum, "Err(kineFactor): dimension of ratio errors and momentum transfers do not match"
 
-kineFactor= np.zeros((binNum, momNum, ratioNum, 2)) #4D?#
+ kineFactor= np.zeros((binNum, momNum, ratioNum, 2)) #4D?#
 
-for b in range(binNum)
+ for b in range(binNum)
     for q in range(momNum)
         Qsq = np.dot(Q[q], Q[q])
         
@@ -30,5 +30,5 @@ for b in range(binNum)
                     / np.repeat[ ratio_err[q], 2).reshape(10,2) \
                     / KK(mEff[b], Qsq, L)
                                 
-  return kineFactor
+return kineFactor
 
