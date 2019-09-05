06d77d01542ab632aed957f6f29b34b5b1af030f
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-09-04 21:19:08 -0400

    Update readwrite.py

--- readwrite.py
+++ readwrite.py
@@ -1,17 +1,17 @@
-def detbinNum(filename): 
-  with open(filename, "r") as file:
-  	for line in file:
-		binNum = len(line.split())
-  return  binNum
+def detbinNum(filename):
+	with open(filename, "r") as file:
+		for line in file:
+		binNum = len(line.split()
+return  binNum
 		
 def readTxtFile (filename, **kwargs):
-   with open(filename, "r") as txtFile:
+	with open(filename, "r") as txtFile:
 		columnNum = len(next(txtfile).split())
 		file.seek(0)
 		lineNum= (txtFile.readlines())
 		txtFile.seek(0)
 		data = np.array(txtFile.read.split(), **kwargs).reshape(lineNum,columnNum)	
-   return data
+return data
 
 def readNthDataCol(filename , N, **kwargs):
 	with open(filename, "r") as file:
@@ -20,23 +20,18 @@ def readNthDataCol(filename , N, **kwargs):
 		lineNum = (file.readlines())
 		file.seek(0)
 	        data = np.array(file.read.split(), **kwargs).reshape(lineNum, columnNum)
-        return data[..., N]
+return data[..., N]
 
 			
-def writeSVDOutputFile( filename, data, Qsq ):
-
-    with open( filename, "w" ) as output:
-
-        for q in range( len( data ) ):
-
-            for r in range( data[ q ].shape[ 0 ] ):
-
-                output.write("{:<10}{:<10}{:<20.10}{:<.10}\n".format(r, \
+def writeSVDOutputFile( filename, data, Qsq ):	
+	with open( filename, "w" ) as output:
+		for q in range( len( data ) ):
+			for r in range( data[ q ].shape[ 0 ] ):
+			output.write("{:<10}{:<10}{:<20.10}{:<.10}\n".format(r, \
                                                                      Qsq[q], \
                                                                      data[q][r,0], \
                                                                      data[q][r,1]))
-
-    print( "Wrote " + filename )
+	print( "Wrote " + filename )
 	
 	
 	
