cc09f182e6df03222693102b131120ca62aa822f
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-09-04 21:35:08 -0400

    Update physQuants.py

--- physQuants.py
+++ physQuants.py
@@ -10,9 +10,8 @@ def kineFactor(ratio_err, mEff, Q, L):
    binNum = len(mEff)
    assert len(Q) == momNum, "Err(kineFactor): dimension of ratio errors and momentum transfers do not match"
    kineFactor= np.zeros((binNum, momNum, ratioNum, 2)) #4D?#
-
-   for b in range(binNum)
-      for q in range(momNum)
+   for b in range(binNum):
+      for q in range(momNum):
         Qsq = np.dot(Q[q], Q[q])
         kineFactor[b, q]= [[ (energy(mEff[b] ,Qsq, L) +mEff[b]), 0], \
                                    [ 2.0 * np.pi / L * Q[ q, 0 ], 0 ], \
