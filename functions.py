4ce9284ce3f0cc47a8b79eccb4001a9235f0b306
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-09-04 21:33:28 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,20 +1,17 @@
 def processMomList (momList):
+   Qsq_s= []
+   Qsq_e= []
+   Qsq = np.round((np.apply_along_axis(np.linalg.norm, 1, np.array(momList)))**2)
 
-Qsq_s= []
-Qsq_e= []
-Qsq = np.round((np.apply_along_axis(np.linalg.norm, 1, np.array(momList)))**2)
+   q_last = Qsq[0]
+   Qsq_s.append(0)
+   Qsq_e.append(0)
 
-q_last = Qsq[0]
-Qsq_s.append(0)
-Qsq_e.append(0)
-
-for q in Qsq[1:]:
-   if q!= q_last:
-      assert q > q_last, "Momentum list not in ascending order"
-      Qsq_s.append(np.where(Qsq == q)[0][0])
-      Qsq_e.append(np.where(Qsq == q)[0][-1])
-      q_last = q; 
-   
+   for q in Qsq[1:]:
+      if q!= q_last:
+         assert q > q_last, "Momentum list not in ascending order"
+         Qsq_s.append(np.where(Qsq == q)[0][0])
+         Qsq_e.append(np.where(Qsq == q)[0][-1])
+         q_last = q; 
    Qsq = sorted(list(set(Qsq)))
-   
-return np.array(Qsq, dtype= int), np.array(Qsq_s), np.array(Qsq_e)
+   return np.array(Qsq, dtype= int), np.array(Qsq_s), np.array(Qsq_e)
