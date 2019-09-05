4a4e75a56a20fa01090f6b53511d2f2faa71fe43
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2019-09-04 21:22:15 -0400

    Update physQuants.py

--- physQuants.py
+++ physQuants.py
@@ -1,22 +1,19 @@
 def KK(mEff, Qsq, L):
-  return np.sqrt( 2* energy(mEff,Q,L) * (energy(mEff, Q,L)+ mEff))
+    return np.sqrt( 2* energy(mEff,Q,L) * (energy(mEff, Q,L)+ mEff))
 
 def energy(mEff, Q, L):
-  return np.sqrt(mEff**2 + (2.0 *np.pi /L)**2 * Qsq)
+    return np.sqrt(mEff**2 + (2.0 *np.pi /L)**2 * Qsq)
 
 def kineFactor(ratio_err, mEff, Q, L):
- momNum = ratio_err.shape[0]
- ratioNum = ratio_err.shape[-1] #multiple error ratios?#
- binNum = len(mEff)
+   momNum = ratio_err.shape[0]
+   ratioNum = ratio_err.shape[-1] #multiple error ratios?#
+   binNum = len(mEff)
+   assert len(Q) == momNum, "Err(kineFactor): dimension of ratio errors and momentum transfers do not match"
+   kineFactor= np.zeros((binNum, momNum, ratioNum, 2)) #4D?#
 
- assert len(Q) == momNum, "Err(kineFactor): dimension of ratio errors and momentum transfers do not match"
-
- kineFactor= np.zeros((binNum, momNum, ratioNum, 2)) #4D?#
-
- for b in range(binNum)
-    for q in range(momNum)
+   for b in range(binNum)
+      for q in range(momNum)
         Qsq = np.dot(Q[q], Q[q])
-        
         kineFactor[b, q]= [[ (energy(mEff[b] ,Qsq, L) +mEff[b]), 0], \
                                    [ 2.0 * np.pi / L * Q[ q, 0 ], 0 ], \
                                    [ 2.0 * np.pi / L * Q[ q, 1 ], 0 ], \
@@ -29,6 +26,5 @@ def kineFactor(ratio_err, mEff, Q, L):
                                    [ 0, -2.0 * np.pi / L * Q[ q, 0 ] ] ] \
                     / np.repeat[ ratio_err[q], 2).reshape(10,2) \
                     / KK(mEff[b], Qsq, L)
-                                
 return kineFactor
 
