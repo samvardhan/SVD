b70b163350157cc0329a6f913729529f52f067cf
Samvardhan Vishnoi <44649832+samvardhan@users.noreply.github.com>
2020-03-02 13:39:00 -0500

    Update physQuants.py

--- physQuants.py
+++ physQuants.py
@@ -2,20 +2,20 @@ import numpy as np
 
 def KK(mEff, Q, L):
     return np.sqrt( 2* energy(mEff,Q,L) * (energy(mEff, Q,L)+ mEff))
-
+#energy value#
 def energy(mEff, Q, L):
     return np.sqrt((mEff*mEff) + (2.0 *(np.pi /L))*(2.0 *(np.pi /L)) * Q)
 
 def kineFactor(ratio_err, mEff, Q, L):
    momNum = ratio_err.shape[0]
-   ratioNum = ratio_err.shape[-1] #multiple error ratios?#
+   ratioNum = ratio_err.shape[-1] #multiple error ratios#
    binNum = len(mEff)
    assert len(Q) == momNum, "Err(kineFactor): dimension of ratio errors and momentum transfers do not match"
-   kineFactor= np.zeros((binNum, momNum, ratioNum, 2)) #4D?#
+   kineFactor= np.zeros((binNum, momNum, ratioNum, 2)) #4D#
    for b in range(binNum):
       for q in range(momNum):
         Qsq = np.dot(Q[q], Q[q])
-        kineFactor[b, q]= [[ (energy(mEff[b] ,Qsq, L) +mEff[b]), 0], \
+        kineFactor[b, q]= [[ (energy(mEff[b] ,Qsq, L) +mEff[b]), 0], \ . #Kinefactor Formula#
                                    [ 2.0 * np.pi / L * Q[ q, 0 ], 0 ], \
                                    [ 2.0 * np.pi / L * Q[ q, 1 ], 0 ], \
                                    [ 2.0 * np.pi / L * Q[ q, 2 ], 0 ], \
